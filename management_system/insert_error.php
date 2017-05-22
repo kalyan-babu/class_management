f11e76ffca1ea65889126ddd73201c88fd0f3b74
kalyan <kalyanbuddavarapu@gmail.com>
2017-05-22 21:22:34 +0530

    file for executing insert error's.

---
+++ management_system/insert_error.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>	
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<?php
+
+	echo'<br><br><br><br><div class="container">
+		<div align="center">
+		<p>Soryy, some errors coming.</p>
+		<p>row is already inserted plese choose UPDATE option rather than INSERT</p>
+		<p>Then <a href="page4.php">click_here</a> to go to ADD_MARKS home page</p>
+		</div></div>';
+	
+?>
+
+</body>
+
+</html>
+
+
